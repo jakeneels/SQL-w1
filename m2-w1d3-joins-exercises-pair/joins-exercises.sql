2fef6e35fa29f1bbcb2e8b505941bd85aa23827b
Jake Neels <nizlmmk@gmail.com>
2018-03-03 14:36:51 -0500

    day 3 pair

--- m2-w1d3-joins-exercises-pair/joins-exercises.sql
+++ m2-w1d3-joins-exercises-pair/joins-exercises.sql
@@ -3,30 +3,60 @@
 
 -- 1. The city name, country name, and city population of all cities in Europe with population greater than 1 million
 -- (36 rows)
-
+--select city.name, country.name, city.population
+-- from city join country on country.code = city.countrycode
+-- where city.population > 1000000 and continent = 'europe';
 -- 2. The city name, country name, and city population of all cities in countries where French is an official language and the city population is greater than 1 million
 -- (2 rows)
-
+--select city.name,country.name, city.population
+--from city join country on city.countrycode = country.code 
+--join countrylanguage on country.code = countrylanguage.countrycode
+--where countrylanguage.language = 'french' and city.population > 1000000 and countrylanguage.isofficial = 'true'
 -- 3. The name of the countries and continents where the language Javanese is spoken
 -- (1 row)
-
+--select country.name, countrylanguage.language
+--from country join countrylanguage on country.code = countrylanguage.countrycode
+--where countrylanguage.language = 'javanese'
 -- 4. The names of all of the countries in Africa that speak French as an official language
--- (5 row)
-
+---- (5 row)
+--select country.name
+--from country join countrylanguage on country.code = countrylanguage.countrycode
+--where country.continent = 'africa' and countrylanguage.language= 'french' and countrylanguage.isofficial = 'true'
 -- 5. The average city population of cities in Europe
 -- (average city population in Europe: 287,684)
+--select avg(city.population) as lul
+--from country join city on country.code = city.countrycode
+--where continent = 'europe'
 
 -- 6. The average city population of cities in Asia
 -- (average city population in Asia: 395,019)
+--select avg(city.population) as lul
+--from country join city on country.code = city.countrycode
+--where continent = 'asia'
 
 -- 7. The number of cities in countries where English is an official language
 -- (number of cities where English is official language: 523)
 
+--select city.name
+--from countrylanguage join city on countrylanguage.countrycode = city.countrycode
+--where language = 'english' and countrylanguage.isofficial = 'true'
+
 -- 8. The average population of cities in countries where the official language is English
--- (average population of cities where English is official language: 285,809)
+ --(average population of cities where English is official language: 285,809)
+--select AVG(city.population)
+--from city join countrylanguage on city.countrycode = countrylanguage.countrycode
+--where countrylanguage.language = 'english'  and countrylanguage.isofficial = 'true'
 
 -- 9. The names of all of the continents and the population of the continent’s largest city
 -- (6 rows, largest population for North America: 8,591,309)
+--select max(city.population), country.continent
+--from country join city on country.code = city.countrycode
+--group by country.continent
 
 -- 10. The names of all of the cities in South America that have a population of more than 1 million people and the official language of each city’s country
 -- (29 rows)
+
+--select city.name, countrylanguage.language
+--from city join country on city.countrycode = country.code
+--join countrylanguage on country.code = countrylanguage.countrycode
+--where country.continent = 'south america' and city.population > 1000000 and countrylanguage.isofficial = 'true'
--- m2-w1d4-constraints-and-transactions-exercises-pair/constraints-and-transactions-exercises-pair.sql
+++ m2-w1d4-constraints-and-transactions-exercises-pair/constraints-and-transactions-exercises-pair.sql
@@ -5,30 +5,82 @@
 -- countrycode is 'USA', and population of 45001. (Yes, I looked it up on 
 -- Wikipedia.)
 
+--insert into city(name, population, countrycode, district)
+--values('smaaallville', 45001, 'usa', 'kansas', 55555)
+
 -- 2. Add Kryptonese to the countrylanguage table. Kryptonese is spoken by 0.0001
 -- percentage of the 'USA' population.
+
+--insert into countrylanguage(language, countrycode, isofficial, percentage)
+--values('kryptonese', 'usa', 1, .0001)
+
+--select countrylanguage.*
+--from countrylanguage
+--where language = 'kryptonese' 
+
 
 -- 3. After heated debate, "Kryptonese" was renamed to "Krypto-babble", change 
 -- the appropriate record accordingly.
 
+--update countrylanguage
+--set language = 'Krypto-babble'
+--where language = 'Kryptonese'
+
 -- 4. Set the US captial to Smallville, Kansas in the country table.
+
+--update country
+--set capital = 6789
+--where code = 'USA'
 
 -- 5. Delete Smallville, Kansas from the city table. (Did it succeed? Why?)
 
+--delete from  city
+--where id = 1337
+
 -- 6. Return the US captial to Washington.
+--select capital from country
+--where country.code = 'usa'
+--update country
+--set capital = 3813
+--where country.code = 'usa'
+
 
 -- 7. Delete Smallville, Kansas from the city table. (Did it succeed? Why?)
+--delete from  city
+--where id = 1337
+--no its already delet
 
 -- 8. Reverse the "is the official language" setting for all languages where the
 -- country's year of independence is within the range of 1800 and 1972 
 -- (exclusive). 
 -- (590 rows affected)
 
+--update countrylanguage 
+--set countrylanguage.isofficial = reverse(countrylanguage.isofficial)
+--from countrylanguage
+--join country on country.code = countrylanguage.countrycode
+--where country.indepyear >= 1800 and country.indepyear <= 1972
+
 -- 9. Convert population so it is expressed in 1,000s for all cities. (Round to
 -- the nearest integer value greater than 0.)
 -- (4079 rows affected)
+
+--update city
+--set population = round(population /1000 , 0)
 
 -- 10. Assuming a country's surfacearea is expressed in miles, convert it to 
 -- meters for all countries where French is spoken by more than 20% of the 
 -- population.
 -- (7 rows affected)
+--select surfacearea , countrylanguage.language, countrylanguage.percentage
+--from country
+--join countrylanguage on countrylanguage.countrycode = country.code
+--where countrylanguage.percentage > 20 and countrylanguage.language= 'french'
+
+
+--is percentage real or not?
+update country
+set surfacearea *= 1609.34 
+from country
+join countrylanguage on countrylanguage.countrycode = country.code
+where countrylanguage.percentage > 20 and countrylanguage.language= 'french'
